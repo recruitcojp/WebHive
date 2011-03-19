f711e9fbc4392361b32af5cbab7774d29476710e
recruitcojp <dwh_core@ml.cocorou.jp>
2011-03-19 18:38:33 +0900

    0.99 beta version / modify config file

--- app/config/core.php
+++ app/config/core.php
@@ -196,7 +196,7 @@
  * CakePHP session IDs are also regenerated between requests if
  * 'Security.level' is set to 'high'.
  */
-	Configure::write('Security.level', 'medium');
+	Configure::write('Security.level', 'low');
 
 /**
  * A random string used in security hashing methods.
