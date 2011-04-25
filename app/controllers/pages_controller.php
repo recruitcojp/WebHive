2e67bb38a6c56f6c7879549840c271c5ca7d3c47
recruitcojp <dwh_core@ml.cocorou.jp>
2011-04-25 10:26:12 +0900

    Version 1.02 commit

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -17,6 +17,16 @@ class PagesController extends AppController {
 		$this->layout='base';
 		$this->user=$this->Auth->user();
 		$user=$this->user;
+
+		//usersテーブル検索
+		$this->loadModel('Users');
+		$username=$user['User']['username'];
+		$users=$this->Users->find('all', array('conditions' => "username='$username'"));
+		if ( count($users) > 0 ){
+			$user['User']['authority']=$users[0]['Users']['authority'];
+		}
+
+		//権限情報が未設定の場合(LDAP認証やWebHiveリポジトリでauthorityが未設定の場合)
 		if ( empty($user['User']['authority']) ){
 			$user['User']['authority']=LDAP_AUTH;
 		}
